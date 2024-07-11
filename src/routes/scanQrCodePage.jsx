a7d518640088866ac645daf0511d0da016e49529
theodorpanguripan <166774123+theodorpanguripan@users.noreply.github.com>
2024-07-11 15:33:09 +0800

    testing qrcode scanner

--- src/routes/scanQrCodePage.jsx
+++ src/routes/scanQrCodePage.jsx
@@ -1,15 +1,18 @@
 import { useEffect, useState} from "react";
 import { Html5Qrcode } from 'html5-qrcode';
+import { Link } from 'react-router-dom';
+import './scanQrCodePage.css';
+
+
 
 
 
 function ScanQrCodePage() {
-    const [result, setResult] = useState("");
+    const [result, setResult] = useState(null);
 
     useEffect(() => {
     
         const scanner  = new Html5Qrcode('reader');
-    
         const config = {
           qrbox: {
             width: 250,
@@ -24,14 +27,15 @@ function ScanQrCodePage() {
         };
     
         scanner.start({ facingMode: "environment" }, config, qrCodeSuccessCallback);
+
       }, []);
     
 
     return(
-        <div className="body alignCenter" style={{flexDirection: "column"}}>
-            <div>Back</div>
-            <div>QR Input:</div>
-            <div>{result}</div>
+        <div style={{flexDirection: "column"}}>
+            <Link to="/">Back</Link>
+            <div>QR Info:</div>
+            <div className="resultContainer">{result}</div>
             <div id="reader" width="600px"></div>
         </div>
     );
